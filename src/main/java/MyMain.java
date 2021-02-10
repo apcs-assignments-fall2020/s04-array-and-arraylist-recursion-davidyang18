0bd6ed7b99416b8c54d1a6d4d9babdd2806c590f
davidyang18 <2817822828@qq.com>
2021-02-10 05:41:41 +0000

    Working methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,33 +1,51 @@
+// David Yang
+// 2/9/2021
+// Worked with Koyo Asakawa on "HasCountCopies" method
 import java.util.ArrayList;
 
 public class MyMain {
 
     // Wrapper method
     public static boolean isSorted(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return false;
+        return isSortedRec(list, 0);
     }
 
     // This recursive method checks if the array is sorted in 
     // non-decreasing order
     public static boolean isSortedRec(ArrayList<Integer> list, int i) {
-        // YOUR CODE HERE
-        return false;
+        if (i == list.size() - 1) {
+            return true;
+        }
+        else if (list.get(i) > (list.get(i + 1))) {
+            return false;
+        }
+        else {
+            return isSortedRec(list , i + 1);
+        }
     }
 
 
 
      // Wrapper method
     public static boolean hasCountCopies(int[] arr, int x, int count) {
-        // YOUR CODE HERE
-        return false;
+        return hasCountCopiesRec(arr, x, count, 0);
     }
 
     // This recursive method checks if the array contains exactly
     // count copies of the integer x
     public static boolean hasCountCopiesRec(int[] arr, int x, int count, int i) {
-        // YOUR CODE HERE
-        return false;
+        if (i == arr.length && count == 0) {
+            return true;
+        }
+        else if (i == arr.length && count != 0) {
+            return false;
+        }
+        else if (arr[i] == x) {
+            return hasCountCopiesRec(arr, x, count - 1, i + 1);
+        }
+        else {
+            return hasCountCopiesRec(arr, x, count, i + 1);
+        }
     }
 
 
@@ -35,13 +53,24 @@ public class MyMain {
 
     // Wrapper method
     public static boolean binarySearch(int[] arr, int num) {
-        // YOUR CODE HERE
-        return false;
+        return binarySearchRec(arr, num, 0, arr.length - 1, 0);
     }
 
     // This recursive method calls binary search on the array
-    public static boolean binarySearchRec(int[] arr, int num, int lowerBound, int upperBound) {        
-        // YOUR CODE HERE
+    public static boolean binarySearchRec(int[] arr, int num, int lowerBound, int upperBound, int middleIndex) {    
+        while (lowerBound <= upperBound) {
+            middleIndex = lowerBound + (upperBound - lowerBound) / 2;
+ 
+            if (arr[middleIndex] > num) {
+                return binarySearchRec(arr, num, lowerBound, middleIndex - 1, middleIndex);
+            }
+            else if (arr[middleIndex] < num) {
+                return binarySearchRec(arr, num, middleIndex + 1, upperBound, middleIndex);
+            }
+            else {
+                return true;
+            }
+        }
         return false;
     }
 
